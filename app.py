abf393eb11315d1f0bcb1139504eb60b73e2e074
nofan190021 <85574272+nofan190021@users.noreply.github.com>
2026-05-05 20:42:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -49,4 +50,5 @@ def index():
     return render_template('index.html', result=result, confidence=confidence)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
